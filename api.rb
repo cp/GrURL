fef531878d608cb72ecaa3f9f34612ca49eba67f
Parker Moore <parkrmoore@gmail.com>
2013-08-13 15:25:49 +0200

    Refactor into Grape helper.

--- api.rb
+++ api.rb
@@ -1,45 +1,81 @@
 require 'grape'
 require 'redis'
+require 'securerandom'
 
-REDIS = Redis.new
+module UrlCache
+  def redis
+    @redis ||= Redis.new
+  end
+
+  def grurl_key(action, key)
+    "GrURL:#{action}:#{key}"
+  end
+
+  def random_key
+    key = ""
+    begin
+      key = SecureRandom.urlsafe_base64
+    end while exists?(key)
+    key
+  end
+
+  def record_click(key)
+    redis.incr(grurl_key(:clicks, key)).to_s
+  end
+
+  def fetch(key, action = :shortened)
+    redis.get(grurl_key(action, key))
+  end
+
+  def save(key, value)
+    redis.set(grurl_key(:shortened, key), value)
+  end
+
+  def exists?(key, action = :shortened)
+    redis.exists(grurl_key(action, key))
+  end
+end
 
 module GrURL
   class API < Grape::API
     format :json
+    helpers UrlCache
 
     get ':key' do
-      url = REDIS.get("shortened-#{params[:key]}")
-      incr("clicks-#{params[:key]}")
+      url = fetch(params[:key])
+      record_click(params[:key])
       redirect url
     end
 
     get ':key/info' do
-      url = REDIS.get("shortened-#{params[:key]}")
-      clicks = REDIS.get("clicks-#{params[:key]}") || '0'
-      return { clicks: clicks, long_url: url }
+      url = fetch(params[:key], :shortened)
+      clicks = fetch(params[:key], :clicks) || '0'
+      { clicks: clicks, long_url: url }
     end
 
     post '/' do
-      if params[:key]
-        if REDIS.exists("shortened-#{params[:key]}")
+      if key = params[:key]
+        if exists?(key)
           error!('Key taken', 500)
         else
-          REDIS.set("shortened-#{params[:key]}", params[:url])
-          return { key: params[:key], short_url: "/#{params[:key]}", long_url: params[:url] }
+          save(key, params.url)
+          {
+            key: key,
+            short_url: "/#{key}",
+            long_url: params.url
+          }
         end
       else
-        key = incr('key')
-        REDIS.set("shortened-#{key}", params[:url])
-        return { key: key, short_url: "/#{key}", long_url: params[:url] }
+        if url = params.url
+          key = random_key
+          save(key, url)
+          { key: key, short_url: "/#{key}", long_url: url }
+        else
+          puts params
+          puts params.url
+          error!('No URL passed', 500)
+        end
       end
     end
   end
-end
-
-private
-
-def incr(key)
-  REDIS.set(key, 0) unless REDIS.exists(key)
-  clicks = REDIS.incr(key).to_s
-  return clicks
 end
